acca5d94d7523455f647f517f7dc3a9707d23f1c
cruelrin <sheveleva_k@iuca.kg>
2024-05-16 19:39:45 +0600

    дизайн  services list

--- workshop/urls.py
+++ workshop/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import service_list, master_list, create_order
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', service_list, name='service_list'),  # URL pattern for the root URL
-      # Keep the URL pattern for service list
+    path('', service_list, name='service_list'),  
     path('masters/', master_list, name='master_list'),
     path('create_order/', create_order, name='create_order'),
-]
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
