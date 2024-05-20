7daa68b0ec81bbbdd6b39c498a80e8b97fe61c83
cruelrin <sheveleva_k@iuca.kg>
2024-05-20 13:17:38 +0600

    все готово

--- workshop/views.py
+++ workshop/views.py
@@ -15,29 +15,31 @@ def master_list(request, service_id=None):
 
 
 
-
-
-
 def create_order(request):
-    if request.method == 'POST':
-        car_model = request.POST['car_model']
-        service_id = request.POST['service']
-        master_id = request.POST['master']
-        service = get_object_or_404(Service, pk=service_id)
-        master = get_object_or_404(Master, pk=master_id)
-        hours_worked = int(request.POST.get('hours_worked', 5))  # Default to 5 hours if not provided
-        total_price = hours_worked * master.price
-        order = Order.objects.create(
-            car_model=car_model,
-            service=service,
-            master=master,
-            hours_worked=hours_worked,
-            total_price=total_price
-        )
-        return render(request, 'workshop/create_order.html', {'order': order})
-    else:
-        master_id = request.GET.get('master_id')
-        service_id = request.GET.get('service_id')
-        master = get_object_or_404(Master, id=master_id)
-        service = get_object_or_404(Service, id=service_id)
-        return render(request, 'workshop/create_order.html', {'master': master, 'service': service})
+    master_id = request.GET.get('master_id')
+    service_id = request.GET.get('service_id')
+
+    master = get_object_or_404(Master, id=master_id)
+    service = get_object_or_404(Service, id=service_id)
+
+   
+    car_model = "Toyota Camry"  
+    hours_worked = 5  
+    hourly_rate = master.price
+    service_cost = service.price
+    total_price = (hourly_rate * hours_worked) + service_cost
+
+    order = {
+        'car_model': car_model,
+        'service': service,
+        'master': master,
+        'hours_worked': hours_worked,
+        'hourly_rate': hourly_rate,
+        'service_cost': service_cost,
+        'total_price': total_price
+    }
+
+    context = {
+        'order': order
+    }
+    return render(request, 'workshop/create_order.html', context)
